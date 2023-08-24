a2bcb837a76bf4c71e5ae4bf2365e7ad8bbe3e73
om patel <omsuperson@gmail.com>
2023-08-24 11:48:14 +0530

    Complete Recursion Exercises

--- Recursion/Main.java
+++ Recursion/Main.java
@@ -3,7 +3,7 @@ package Recursion;
 public class Main {
     public static void main(String[] args) {
         Main r = new Main();
-        var t = r.sumOfDigits(31);
+        var t = r.decToBinary(10);
         System.out.println(t);
 
     }
@@ -36,8 +36,38 @@ public int sumOfDigits(int n) {
         return 0;
     }
 
-    return n%10 + sumOfDigits(n/10);
+    return (n%10 + sumOfDigits(n/10));
 }
 
+public int power(int n, int exp) {
+    if (exp < 0) {
+        return -1;
+    }
+    if (exp == 0) {
+        return 1;
+    }
+    if (exp == 1) {
+        return n;
+    }
+    return n * power(n, exp-1);
+}
+
+public int gcd(int n, int m) {
+    if (n<0 || m<0) {
+        return -1;
+    }
+
+    if (m == 0) {
+        return n;
+    }
+    return gcd(m, n%m);
+}
+
+public int decToBinary(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return n%2 + (10*decToBinary(n/2));
+}
 
 }
